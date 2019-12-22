1286715ffafb2917721e38b52f16018df092da40
jesseleeruiz <jesseleeruiz@gmail.com>
2019-12-22 13:10:03 -0800

    Added View Controllers.

---
+++ Side Menu/Side Menu/View Controllers/SideMenuTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SideMenuTableViewController.swift
+//  Side Menu
+//
+//  Created by Jesse Ruiz on 12/22/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuTableViewController: UITableViewController {
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
