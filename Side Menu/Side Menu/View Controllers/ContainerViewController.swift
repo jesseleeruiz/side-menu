30c536c274b344594948e8c58efb15831e9cb794
jesseleeruiz <jesseleeruiz@gmail.com>
2020-01-24 15:03:38 -1000

    Added sideMenuFunc and made adjustments to container view in main storyboard

--- Side Menu/Side Menu/View Controllers/ContainerViewController.swift
+++ Side Menu/Side Menu/View Controllers/ContainerViewController.swift
@@ -12,22 +12,18 @@ class ContainerViewController: UIViewController {
     
     // MARK: Outlets
     @IBOutlet weak var sideMenuConstraint: NSLayoutConstraint!
+    var sideMenuOpen: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: Methods
+    func toggleSideMenu() {
+        if sideMenuOpen {
+            sideMenuConstraint.constant = -285
+        } else {
+            sideMenuConstraint.constant = 0
+        }
     }
-    */
-
 }
